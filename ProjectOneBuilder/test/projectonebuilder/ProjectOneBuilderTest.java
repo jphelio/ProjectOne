34380210ca3b752f4f5d59e95ae07324091e9d22
jphelio <juhani.helio@helsinki.fi>
2014-08-19 12:03:49 +0300

    added ProjectOneBuilder

---
+++ ProjectOneBuilder/test/projectonebuilder/ProjectOneBuilderTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projectonebuilder;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Juhani Heliö
+ */
+public class ProjectOneBuilderTest {
+    
+    public ProjectOneBuilderTest() {
+    }
+
+    /**
+     * Test of main method, of class ProjectOneBuilder.
+     */
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+    
+}
